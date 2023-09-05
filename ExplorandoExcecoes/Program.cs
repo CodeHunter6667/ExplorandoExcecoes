d14af4e3e505e4c01cbc12cfa68453841ad5f18d
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-09-05 15:54:07 -0300

    Adicionar arquivos de projeto.

---
+++ ExplorandoExcecoes/Program.cs
@@ -0,0 +1,15 @@
+﻿
+try
+{
+    string[] linha = File.ReadAllLines("D:\\Projetos Programacao\\Projetos C Sharp\\Projetos Dio\\ExcecoesEColecoes\\ExplorandoExcecoes\\NovaPasta\\Arquivo_Leitura.txt");
+
+    foreach (string linhas in linha)
+    {
+        Console.WriteLine(linhas);
+
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma excecao generica {ex.Message}");
+}
